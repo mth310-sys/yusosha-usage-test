205972fc9c775c55c843611a5f4dea2e6eaa2e78
mth310-sys <mth.310@gmail.com>
2026-08-11 09:01:46 +0900

    feat: add Lupin B4 phase 0 config

---
+++ test_lupin_b4/js/config.js
@@ -0,0 +1,11 @@
+export const CONFIG = Object.freeze({
+  initialCredit: 50,
+  maxBet: 3,
+  setting: 1,
+  logLimit: 120
+});
+
+export const PHASE = Object.freeze({
+  WAIT_BET: 'WAIT_BET',
+  WAIT_LEVER: 'WAIT_LEVER'
+});
