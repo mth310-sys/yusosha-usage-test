71a4ea177d5e369f71d209c2fc66bf07c6670a90
mth310-sys <mth.310@gmail.com>
2026-08-12 01:40:53 +0900

    Step 6Z refine Treasure RUSH verified 4-9G behavior

--- test_lupin_b4/js/treasure-rush-profile.js
+++ test_lupin_b4/js/treasure-rush-profile.js
@@ -4,13 +4,15 @@
 export const TREASURE_RUSH_PROFILE = Object.freeze({
   entryTrigger: 'TREASURE_HUNT_SUCCESS',
   minimumGames: 4,
+  maximumGames: 9,
   averageTreasurePoints: 499000,
+  everyGameTreasureAdd: true,
+  continuationCue: 'MADAMADA',
   source: 'VERIFIED_PUBLISHED_TREASURE_RUSH_OUTLINE',
   unresolved: Object.freeze({
     entryRate: true,
-    continuationDistribution: true,
-    perGameTreasureDistribution: true,
-    exactEndConditionBeyondMinimum4G: true
+    durationDistribution4To9: true,
+    perGameTreasureDistribution: true
   }),
   policy: 'NO_AUTO_ENTRY_OR_SYNTHETIC_DISTRIBUTION_UNTIL_VERIFIED'
 });
@@ -19,7 +21,10 @@ export function treasureRushSnapshot() {
   return {
     entryTrigger: TREASURE_RUSH_PROFILE.entryTrigger,
     minimumGames: TREASURE_RUSH_PROFILE.minimumGames,
+    maximumGames: TREASURE_RUSH_PROFILE.maximumGames,
     averageTreasurePoints: TREASURE_RUSH_PROFILE.averageTreasurePoints,
+    everyGameTreasureAdd: TREASURE_RUSH_PROFILE.everyGameTreasureAdd,
+    continuationCue: TREASURE_RUSH_PROFILE.continuationCue,
     unresolved: { ...TREASURE_RUSH_PROFILE.unresolved },
     policy: TREASURE_RUSH_PROFILE.policy
   };
