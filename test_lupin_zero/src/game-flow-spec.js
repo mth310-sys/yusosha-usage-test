4e19cbb16ed5ebb29c8a879cd44d586b34b4ee04
mth310-sys <mth.310@gmail.com>
2026-08-23 10:51:14 +0900

    feat: connect Lupin Bonus success flow

--- test_lupin_zero/src/game-flow-spec.js
+++ test_lupin_zero/src/game-flow-spec.js
@@ -34,14 +34,17 @@ export const GAME_FLOW_SPEC = Object.freeze({
     Object.freeze({ from: GameMode.GOLDEN_TIME, trigger: 'TREASURE_REACHES_1000000', to: GameMode.EXTRA_BONUS, evidence: FlowEvidence.VERIFIED_LINK }),
     Object.freeze({ from: GameMode.EXTRA_BONUS, trigger: 'GOLD_SEVEN_ALIGNED', to: GameMode.GOLD_RUSH, evidence: FlowEvidence.VERIFIED_LINK }),
     Object.freeze({ from: GameMode.GOLDEN_TIME, trigger: 'TREASURE_BATTLE_LOSS_PULLBACK_HIT', to: GameMode.REVENGE_CHANCE, evidence: FlowEvidence.VERIFIED_LINK }),
-    Object.freeze({ from: GameMode.REVENGE_CHANCE, trigger: 'PULLBACK_ANNOUNCEMENT_COMPLETE', to: GameMode.LUPIN_BONUS, evidence: FlowEvidence.VERIFIED_LINK })
+    Object.freeze({ from: GameMode.REVENGE_CHANCE, trigger: 'PULLBACK_ANNOUNCEMENT_COMPLETE', to: GameMode.LUPIN_BONUS, evidence: FlowEvidence.VERIFIED_LINK }),
+    Object.freeze({ from: GameMode.LUPIN_BONUS, trigger: 'ZENIGATA_BATTLE_WIN_OR_REVIVAL', to: GameMode.GOLDEN_TIME, evidence: FlowEvidence.VERIFIED_LINK })
   ]),
   knownButUnresolved: Object.freeze([
     Object.freeze({ mode: GameMode.WANTED_CHANCE, fact: 'LIQUID_REEL_CHANCE_EYE_RATES_KNOWN', evidence: 'MULTI_SOURCE_MATCH', automaticEntryRoute: null, automaticEntryProbability: null }),
     Object.freeze({ mode: GameMode.ODOROBO_ZONE, fact: 'SUCCESS_DESTINATION_IS_LUPIN_BONUS_OR_GOLDEN_TIME', evidence: 'MULTI_SOURCE_MATCH', exactBonusVsArtSplit: null }),
     Object.freeze({ mode: GameMode.FUJIKO_ZONE, fact: 'SUCCESS_DESTINATION_IS_LUPIN_BONUS_OR_GOLDEN_TIME', evidence: 'MULTI_SOURCE_MATCH', exactBonusVsArtSplit: null }),
     Object.freeze({ mode: GameMode.TREASURE_RUSH, fact: 'ENTRY_ON_TREASURE_HUNT_SUCCESS', evidence: 'MULTI_SOURCE_MATCH', automaticTreasureHuntOccurrenceRate: null, treasureHuntSuccessRate: null }),
-    Object.freeze({ mode: GameMode.REVENGE_CHANCE, fact: 'TEN_GAME_PULLBACK_ANNOUNCEMENT_AFTER_TREASURE_BATTLE_LOSS', evidence: 'PUBLISHED_ANALYSIS', perGameSuccessRate: null })
+    Object.freeze({ mode: GameMode.REVENGE_CHANCE, fact: 'TEN_GAME_PULLBACK_ANNOUNCEMENT_AFTER_TREASURE_BATTLE_LOSS', evidence: 'PUBLISHED_ANALYSIS', perGameSuccessRate: null }),
+    Object.freeze({ mode: GameMode.LUPIN_BONUS, fact: 'FAILURE_MAY_ROUTE_TO_REVENGE_CHANCE', evidence: 'MULTI_SOURCE_MATCH', revengeEntryRate: null }),
+    Object.freeze({ mode: GameMode.LUPIN_BONUS, fact: 'ART_EXPECTATION_ABOUT_50_PERCENT_BUT_EXACT_PER_ROLE_LOTTERY_UNRESOLVED', evidence: 'PUBLISHED_ANALYSIS', exactPerRoleLottery: null })
   ]),
   policy: Object.freeze({
     inferMissingLinks: false,
